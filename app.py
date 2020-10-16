ea229c5a5f64ef0eb1afdec90654a70b309560a8
Reuben Cleetus <rcleetus@gmail.com>
2020-10-16 14:35:13 -0400

    Added route

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+
+@app.route('/hello/<name>')
+def hello_name(name="Anonymous"):
+    return f"Hello, {name}!"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)

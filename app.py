eecf2c6f3c1932ce6f461e16fd0447d394aea14f
Reuben Cleetus <rcleetus@gmail.com>
2020-10-16 14:39:07 -0400

    v2

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-
 @app.route('/hello/<name>')
 def hello_name(name="Anonymous"):
     return f"Hello, {name}!"
+
+@app.route('/version')
+def version():
+    return "v2"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)

46af5a9638ba1799ad9f29fb71bb53d5c976efd2
Reuben Cleetus <rcleetus@gmail.com>
2021-09-19 17:08:14 -0400

    Updated templates

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is v4!'
+    #return 'Hello, World! This is v4!'
+    return render_template('index.html', msg='Hello World!', title="Hello from a Docker Container!")
 
 @app.route('/hello/<name>')
 def hello_name(name):
-    return "Hello, " + name
+    return render_template('index.html', msg=f"Hello, {name}!", title="Hello from a Docker Container!")
+    #return "Hello, " + name
 
 
 @app.route('/universe')
 def universe():
     return "42"
-
-
-
-@app.route('/version')
-def version():
-    return "v3"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)

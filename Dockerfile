c78efb519c6906c3b83dffc79bf3c83a47cdaf5b
Reuben Cleetus <rcleetus@gmail.com>
2021-09-19 18:27:19 -0400

    updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM ubuntu:16.04
+FROM ubuntu:latest
 RUN apt-get update -y
-RUN apt-get install -y python-pip python-dev build-essential
-RUN rm -rf /var/lib/apt/lists
+RUN apt-get install -y python3.9
+RUN apt-get install -y pip python-dev build-essential
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-ENTRYPOINT ["python"]
+ENTRYPOINT ["python3"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -7,11 +7,6 @@ def hello():
     #return 'Hello, World! This is v4!'
     return render_template('index.html', msg='Hello World!', title="Hello from a Docker Container!")
 
-@app.route('/hello/<name>')
-def hello_name(name):
-    return render_template('index.html', msg=f"Hello, {name}!", title="Hello from a Docker Container!")
-    #return "Hello, " + name
-
 
 @app.route('/universe')
 def universe():
